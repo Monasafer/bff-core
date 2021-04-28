4b02749d9b4deff8c4b2464cfdf8975dddd08ce6
FedeM <federicomarilungo@gmail.com>
2021-04-27 21:42:40 -0300

    heroku database config

--- src/connection.js
+++ src/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 let conexion = mysql.createConnection(
     {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'monadb'
+        host: 'us-cdbr-east-03.cleardb.com',
+        user: 'bc74c327788e47',
+        password: '8dcf085d',
+        database: 'heroku_b667af063c5f73a'
     }
 );
 conexion.connect( 
