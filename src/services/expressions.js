097133097e233136d41c9adf493c5ecafb7bb48f
Lysholm-Mariano <mariano.lysholm@gmail.com>
2021-12-12 23:14:06 -0300

    Regex Validation
    No admitia tildes

--- src/services/expressions.js
+++ src/services/expressions.js
@@ -2,7 +2,7 @@ const expresiones = {
     userValidation: /^[a-zA-Z0-9\_\-]{7,20}$/, // Letters, numbers, dash, and underscore
     passValidation: /^[a-zA-Z0-9\_\-]{7,20}$/, // 7 to 20 digits.
     mailValidation: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-    nameValidation: /^[-\w\s]+$/, // Alphanumeric and space
+    nameValidation: /^[a-zA-Z0-9\u00C0-\u017F\s]+$/, // Alphanumeric and space
     bool: /^[01]$/, //Zero or one
     invalidNumber: 'Must be a numeric value',
     invalidDate: 'Invalid date entered',
